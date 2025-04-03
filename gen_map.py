70271537d2a2e25a9c7bdb1f01822fb04c3444a9
Charlie Weiss <csweiss45@gmail.com>
2025-04-03 01:34:58 -0400

    added gen_map.py

---
+++ gen_map.py
@@ -0,0 +1,14 @@
+from csv_manipulation import *
+
+
+
+
+def main():
+    print("main")
+    time_series_dict = get_time_series_dict(read_csv(CSV_PATH), 100)
+    print(time_series_dict)
+
+
+
+if __name__ == "__main__":
+    main()
