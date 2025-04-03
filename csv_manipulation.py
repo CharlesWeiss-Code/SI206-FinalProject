36bc6fb5743725e04ff93ebf4bb950dc233839c5
Charlie Weiss <csweiss45@gmail.com>
2025-04-03 01:01:59 -0400

    first real code for csv_manipulation

--- csv_manipulation.py
+++ csv_manipulation.py
@@ -1,3 +1,17 @@
 """
 handles csv data
 """
+
+import csv
+
+def read_csv(path):
+    with open(path, 'r') as f:
+        reader = csv.reader(f)
+        return list(reader)
+    
+
+def main():
+    print(read_csv)
+
+if __name__ == "__main__":
+    main()
