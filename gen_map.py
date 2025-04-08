36ec1d6d81ccc7699ee83261556da3631a6acfeb
Charlie Weiss <csweiss45@gmail.com>
2025-04-07 22:06:26 -0400

    satelite

--- gen_map.py
+++ gen_map.py
@@ -3,19 +3,46 @@ import folium.plugins
 from datetime import datetime, timedelta
 import csv_manipulation
 import os
+import json
 
-data = csv_manipulation.get_time_series_dict(csv_manipulation.read_csv(), skip_value=100)
+skip_value = 20
+
+data = csv_manipulation.get_time_series_dict(csv_manipulation.read_csv(), skip_value=skip_value)
 
 # print(data)
 
 start_date = datetime(year=2020,month=1,day=22)
 
 time_index = [
-    (start_date + k * timedelta(1)).strftime("%Y-%m-%d") for k in range(len(data))
+    (start_date + k * timedelta(1)*skip_value).strftime("%Y-%m-%d") for k in range(len(data))
 ]
-m = folium.Map([44.9672,-98.50], zoom_start=6)
+m = folium.Map([39.8333, -98.5833], zoom_start=4,max_zoom=7,min_zoom=4)
 
-hm = folium.plugins.HeatMapWithTime(data,index=time_index,auto_play=True,max_opacity=0.3)
+hm = folium.plugins.HeatMapWithTime(data,index=time_index,auto_play=True,max_opacity=0.5, name="Covid-19 Heatmap",blur=1)
+
+folium.TileLayer(
+    tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    attr='Esri',
+    name='Esri Satellite',
+    overlay=False,
+    control=True
+).add_to(m)
+
+geojson_path = "./data/us-states.json"  # Adjust the path to your GeoJSON file
+with open(geojson_path, 'r') as f:
+    state_borders = json.load(f)
+
+# Add the state borders to the map
+folium.GeoJson(
+    state_borders,
+    name="State Borders",
+    style_function=lambda x: {
+        "color": "white",  # Border color
+        "weight": 1,  # Border thickness
+        "opacity": 0.5,  # Border opacity
+        "fillOpacity": 0  # No fill, just the borders
+    },
+).add_to(m)
 
 hm.add_to(m)
 
